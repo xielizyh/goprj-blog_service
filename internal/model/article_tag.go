8c799c50b70d55bc3f1197a2708a7be8fea5f45e
xieli <xieliyzh@163.com>
2021-11-07 17:22:59 +0800

    feat(article): add article db deal

--- internal/model/article_tag.go
+++ internal/model/article_tag.go
@@ -2,6 +2,7 @@ package model
 
 import "github.com/jinzhu/gorm"
 
+// ArticleTag 文章和标签的关联模型
 type ArticleTag struct {
 	*Model
 	TagID     uint32 `json:"tag_id"`
@@ -12,6 +13,65 @@ func (a ArticleTag) TableName() string {
 	return "blog_article_tag"
 }
 
-func (at ArticleTag) Create(db *gorm.DB) error {
-	return db.Create(&at).Error
+// GetByAID 根据文章ID获取
+func (at ArticleTag) GetByAID(db *gorm.DB) (ArticleTag, error) {
+	var articleTag ArticleTag
+	err := db.Where("article_id = ? AND is_del = ?", at.ArticleID, 0).First(&articleTag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return articleTag, err
+	}
+
+	return articleTag, nil
+}
+
+// ListByTID 根据标签ID获取
+func (at ArticleTag) ListByTID(db *gorm.DB) ([]*ArticleTag, error) {
+	var articleTags []*ArticleTag
+	if err := db.Where("tag_id = ? AND is_del = ?", at.TagID, 0).Find(&articleTags).Error; err != nil {
+		return nil, err
+	}
+
+	return articleTags, nil
+}
+
+func (a ArticleTag) ListByAIDs(db *gorm.DB, articleIDs []uint32) ([]*ArticleTag, error) {
+	var articleTags []*ArticleTag
+	err := db.Where("article_id IN (?) AND is_del = ?", articleIDs, 0).Find(&articleTags).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+
+	return articleTags, nil
+}
+
+func (a ArticleTag) Create(db *gorm.DB) error {
+	if err := db.Create(&a).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a ArticleTag) UpdateOne(db *gorm.DB, values interface{}) error {
+	if err := db.Model(&a).Where("article_id = ? AND is_del = ?", a.ArticleID, 0).Limit(1).Updates(values).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a ArticleTag) Delete(db *gorm.DB) error {
+	if err := db.Where("id = ? AND is_del = ?", a.Model.ID, 0).Delete(&a).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a ArticleTag) DeleteOne(db *gorm.DB) error {
+	if err := db.Where("article_id = ? AND is_del = ?", a.ArticleID, 0).Delete(&a).Limit(1).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
